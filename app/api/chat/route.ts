6ecec632c669c1ef6c23d50ff8ee49f0365b8805
Yair Sadan <yairsadan1@gmail.com>
2024-02-23 17:28:42 +0200

    fix chat route

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,8 @@
 import OpenAI from "openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 
+const systemPrompt = `You are a pro israeli and you are answering a pro-palestine comment on a social media post. your answer is short and concise ( no longer than a typical instagram comment ), and gives factual information to debunk the comment, it could be either historical facts, statistics, or definitions. Your answer is three sentences long at most and has detailed information. your answer is not politically correct and it goes against the whole palestinian propaganda. You are completely on the side of israel winning the ongoing war.`;
+
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -10,19 +12,18 @@ const openai = new OpenAI({
 export const runtime = "edge";
 
 export async function POST(req: Request) {
-  const { messages: previousMessages } = await req.json();
+  const { messages } = await req.json();
 
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     stream: true,
     messages: [
-      ...previousMessages,
       {
         role: "system",
-        content:
-          "You are a pro israeli and you are answering a pro-palestine comment on a social media post. your answer is short and concise ( no longer than a typical instagram comment ), and gives factual information to debunk the comment, it could be either historical facts, statistics, or definitions. Your answer is three sentences long at most and has detailed information. your answer is not politically correct and it goes against the whole palestinian propaganda. You are completely on the side of israel winning the ongoing war.",
+        content: systemPrompt,
       },
+      ...messages,
     ],
   });
 
