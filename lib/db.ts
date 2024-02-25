c4aaaa8d14f0dcd86a013a01fe7f1094660b5437
Yair Sadan <yairsadan1@gmail.com>
2024-02-25 12:27:20 +0200

    Update db.ts to use accelerate

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,41 @@
-import { PrismaClient } from "@prisma/client/edge";
-import {withAccelerate} from '@prisma/extension-accelerate'
+import { PrismaClient } from '@prisma/client/edge'; // Import from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate';
+import 'server-only';
 
-declare global {
-  var prisma: PrismaClient | undefined;
+const createStandardPrismaClient = () => {
+	return new PrismaClient({
+		log:
+			process.env.NODE_ENV === 'development'
+				? ['query', 'error', 'warn']
+				: ['error'],
+	});
+};
+
+// Singleton pattern for creating an accelerated PrismaClient instance.
+const createAcceleratedPrismaClient = () => {
+	return new PrismaClient({
+		log:
+			process.env.NODE_ENV === 'development'
+				? ['query', 'error', 'warn']
+				: ['error'],
+	}).$extends(withAccelerate());
+};
+
+// Define a type for the accelerated client.
+type PrismaClientAccelerated = ReturnType<typeof createAcceleratedPrismaClient>;
+
+const globalForPrisma = globalThis as unknown as {
+	standardPrisma: PrismaClient | undefined;
+	acceleratedPrisma: PrismaClientAccelerated | undefined;
+};
+
+const db = globalForPrisma.standardPrisma ?? createStandardPrismaClient();
+const acceleratedDb =
+	globalForPrisma.acceleratedPrisma ?? createAcceleratedPrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+	globalForPrisma.standardPrisma = db;
+	globalForPrisma.acceleratedPrisma = acceleratedDb;
 }
 
-export const db = globalThis.prisma || new PrismaClient().$extends(withAccelerate());
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+export { db, acceleratedDb };
