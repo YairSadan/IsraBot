b0e91bb7e631d64ebe89ae92a1efe36ebb5d75b3
Yair Sadan <yairsadan1@gmail.com>
2024-02-25 11:31:58 +0200

    Sort classes

--- app/(main)/[userId]/page.tsx
+++ app/(main)/[userId]/page.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const UserPage = () => {
   return (
-    <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
-      <h1 className="text-xl leading-none font-bold text-center md:text-[300px]">
+    <div className="relative bg-gradient-to-r from-primary to-secondary-foreground bg-clip-text text-transparent">
+      <h1 className="text-center text-xl font-bold leading-none md:text-[300px]">
         IsraBot
       </h1>
     </div>
