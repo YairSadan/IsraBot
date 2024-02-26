d84a007708d5b4c7f85024aec9a082fbb353793a
Yair Sadan <yairsadan1@gmail.com>
2024-02-26 16:06:23 +0200

    Fix file path in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ["/api/completion", "/", '/sign-in', '/sign-up', '/contact' , '/about', '/israelIcon.svg', 'israelIcon.png'],
+  publicRoutes: ["/api/completion", "/", '/sign-in', '/sign-up', '/contact' , '/about', '/israelIcon.svg', '/israelIcon.png'],
   // Routes that can always be accessed, and have
   // no authentication information
 });
