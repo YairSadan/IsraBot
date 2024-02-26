3c5bfec603784e2321680f03b9047e790bdddf45
Yair Sadan <yairsadan1@gmail.com>
2024-02-26 16:20:47 +0200

    fix

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,10 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: [ '/sign-in', '/sign-up', ],
+  publicRoutes: ['/', '/sign-in', '/sign-up', ],
   // Routes that can always be accessed, and have
   // no authentication information
-  ignoredRoutes: ["/api/completion",'/','/contact' , '/about', '/israelIcon.svg', '/israelIcon.png', '/favicon.ico']
+  ignoredRoutes: ["/api/completion",'/contact' , '/about', '/israelIcon.svg', '/israelIcon.png', '/favicon.ico']
 });
 
 export const config = {
